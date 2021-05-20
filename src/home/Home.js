9d91b76e1d9e303283bd8c5cff85ead209d6b96a
taylor-tokareff <taylortokareff@gmail.com>
2021-05-20 14:48:34 -0700

    fav working

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,60 +8,63 @@ const WHITE_HEART = '🤍';
 const RED_HEART = '❤️';
 
 export default class Home extends Component {
-  
-state = {
-  search: '',
-  gifs: [],
-  favorites: []
-}
+
+  state = {
+    search: '',
+    gifs: [],
+    favorites: []
+  }
 
 
-handleSearchInput = ({ target }) => {
-  this.setState({ search: target.value });
-}
+  handleSearchInput = ({ target }) => {
+    this.setState({ search: target.value });
+  }
 
-handleSearch = async () => {
-  const { search } = this.state;
-  const { userToken } = this.props;
-  const response = await request.get('/api/gifs')
-    .set('Authorization', userToken)
-    .query({ search: search });
- 
-  this.setState({ gifs: response.body });
-}
+  handleSearch = async () => {
+    const { search } = this.state;
+    const { userToken } = this.props;
+    const response = await request.get('/api/gifs')
+      .set('Authorization', userToken)
+      .query({ search: search });
 
-handleFavoriteAdd = async ({ target }) => {
-  const { favorites } = this.state;
-  const updateFavorites = [...favorites, target.value];
-  this.setState({ favorites: updateFavorites });
-}
+    this.setState({ gifs: response.body });
+  }
 
-render() {
-  const { gifs } = this.state;
-  
-  return (
-    <div className="Home">
-      <div className="search">
-        <input placeholder='Search for a Gif!' value={this.state.search} onChange={this.handleSearchInput}/>
-        <button onClick={this.handleSearch} >Search!</button>
+  handleFavoriteAdd = async ({ target }) => {
+    const { favorites, gifs } = this.state;
+    const matchingGif = gifs.find(gif => gif.id === target.value);
+    const newFavorite = await addFavorite(matchingGif);
+    const updateFavorites = [...favorites, matchingGif];
+    this.setState({ favorites: updateFavorites });
+  }
+
+  render() {
+    const { gifs } = this.state;
+
+
+    return (
+      <div className="Home">
+        <div className="search">
+          <input placeholder='Search for a Gif!' value={this.state.search} onChange={this.handleSearchInput} />
+          <button onClick={this.handleSearch} >Search!</button>
+        </div>
+
+        <ul>
+          {gifs.map(gif => (
+            <li key={gif.id}>
+              <img src={gif.images.original.url} alt={gif.title}></img>
+              <button onClick={this.handleFavoriteAdd} className="heart" value={gif.id}>
+                {this.state.favorites.find(item => item.id === gif.id)
+                  ? RED_HEART
+                  : WHITE_HEART}
+              </button>
+            </li>
+          ))}
+
+        </ul>
+
       </div>
-        
-      <ul>
-        {gifs.map(gif => (
-          <li key={gif.id}>
-            <img src={gif.images.original.url} alt={gif.title}></img>
-            <button onClick={this.handleFavoriteAdd} className="heart" value={gif.id}>
-              {this.state.favorites.find(item => item === gif.id)
-                ? RED_HEART
-                : WHITE_HEART}
-            </button>
-          </li>
-        ))}
-        
-      </ul>
-
-    </div>
-  );
-}
+    );
+  }
 
 }
